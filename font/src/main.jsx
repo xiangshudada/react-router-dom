b33f47e9597b0e94255206a9d6384ae0ee7d0d2c
zlpei <812476105@qq.com>
2024-04-08 17:13:29 +0800

    first commit

---
+++ font/src/main.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  action as contactAction,
+  loader as contactLoader,
+} from "./routes/contact";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+      },
+    ],
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
---
+++ font/src/routes/root.jsx
@@ -0,0 +1,108 @@
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation, useSubmit } from "react-router-dom";
+import { getContacts, createContact } from "../conjacts";
+import { useEffect } from "react";//添加useEffect
+
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };//添加q
+}
+
+export default function Root(props) {
+    const { contacts, q } = useLoaderData();//使用q
+    const submit = useSubmit();//添加submit
+    const navigation = useNavigation();
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );//添加searching
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);//添加useEffect
+    return (
+        <>
+            <div id="sidebar">
+                <h1>React Router Contacts</h1>
+                <div>
+                    <Form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                            defaultValue={q}//使用q
+                            onChange={(event) => {
+                                //修改：添加replace
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={!searching}
+                        />
+                        <div
+                            className="sr-only"
+                            aria-live="polite"
+                        ></div>
+                    </Form>
+                    <Form method="post">
+                        <button type="submit">New</button>
+                    </Form>
+                </div>
+                <nav>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                    ? "pending"
+                                                    : ""
+                                        }
+                                    >
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
+                </nav>
+            </div>
+            <div
+                id="detail"
+                className={
+                    navigation.state === "loading" ? "loading" : ""
+                }
+            >
+                <Outlet />
+            </div>
+        </>
+    );
+}
